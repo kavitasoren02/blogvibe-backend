ca9e1c0cd5a5e9f289a7b603792ccc83c57051a1
kavita <kavitasoren2000@gmail.com>
2025-07-17 01:08:09 +0530

    first commit

--- index.js
+++ index.js
@@ -16,7 +16,10 @@ const app = express()
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(cors());
+app.use(cors({
+    origin: 'https://blogvibeweb.netlify.app',
+    credentials: true, 
+  }));
   
 app.use("/", mainRoutes);
 
